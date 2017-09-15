709055f4a604dbeabd2ca696e17c03468dcb2d21
dominik.mozny <dominik.mozny@adesso.ch>
2017-09-15 15:37:18 +0200

    Fetch questions after page is loaded

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
 import React, {Component} from 'react';
+import {connect} from 'react-redux'
 import './App.css';
 import RemoteMsgRedux from "./containers/RemoteMsgRedux";
 import WordFormRedux from "./containers/QuestionFormRedux";
 import QuestionsAndAnswersRedux from "./containers/QuestionsAndAnswersRedux";
+import {fetchQuestionsToBeAnswered} from "./actions/index";
 
 class App extends Component {
+    componentDidMount() {
+        this.props.dispatch(fetchQuestionsToBeAnswered())
+    }
+
     render() {
         return (
             <div>
@@ -16,4 +22,4 @@ class App extends Component {
     }
 }
 
-export default App;
+export default connect()(App);
